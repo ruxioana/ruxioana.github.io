a25327bf65767eac98da9789b9c186bcbf8f8e17
ruxioana <ruxy24@gmail.com>
2020-05-24 17:43:13 +0300

    xo game + link style

--- x0/xo.js
+++ x0/xo.js
@@ -1,3 +1,5 @@
+Vue.config.devtools = true;
+
 var app = new Vue({
     el: '#app',
     data: {
@@ -19,7 +21,7 @@ var app = new Vue({
             // schimbam jucatorul activ
             this.activePlayer = this.activePlayer == 'X' ? 'O' : 'X';
             // verificam daca s-a terminat jocul 
-            this.checkFinished();
+            this.checkComplete();
         },
         checkEndGame() {
             this.checkComplete(this.table.filter((value, index) => index < 3)); /* se verifica daca ai castigat pe prima linie */
@@ -69,5 +71,39 @@ function checkEndGame() {
 function checkComplete(cells) {
     if(cells.every(cell => $(cell).text() == 'X') || cells.every(cell => $(cell).text() == 'O')) { /* $(cell).text() ia textul din paranteze, adica cell, pentru a verifica daca este egal cu X sau O */
         $('.end').addClass('visible');
+        activePlayer = '';
     }
-}
+} 
+
+const quotes = [
+    '"Nimic nu valorează mai mult decât ziua de azi.” – Goethe',
+    '“O faptă bună este precum un clopot care cheamă oamenii la închinare.” – proverb danez',
+    '“Întoarce-ți fața către soare și umbrele vor rămâne în urma ta.” – proverb din Noua Zeelandă',
+    '“Pot rezuma în două cuvinte tot ce am învățat despre viață: merge înainte.” – Robert Frost',
+    '“Viața este ceva ce faci când nu te poți duce la culcare.” – Fran Lebowitz',
+    '“Cei doi mari inamici ai fericirii sunt durerea și plictiseala.” – Arthur Schopenhauer',
+    '“A avea un prieten este mai vital decât a avea un înger.” – Nichita Stănescu',
+    '“Copilăria e cadoul pe care ni-l dă viaţa.” – Horaţiu Mălăele',
+    '“Fericirea este ca o minge: alergăm după ea și cum am prins-o îi dăm cu piciorul.” – Duisseux',
+    '“Viața este ca mersul pe bicicletă. Pentru a-ți menține echilibrul trebuie să continui să mergi inainte.” – Albert Einstein',
+    '“Când puterea de iubire va depăși iubirea de putere, în lume va fi pace.” - Jimi Hendrix',
+    '“Valorezi atât cât te apreciezi.” – F. Rabelais',
+    '“Glumind putem spune orice, chiar şi adevărul.” – Sigmund Freud',
+    '“Gândiţi critic şi nu memoraţi doar ceea ce vor alţii să gândiţi.” – N.D. Walsch',
+    '“Ceea ce nu trăim la timp, nu trăim niciodată.” - Octavian Paler',
+    '“Nu permite ca opiniile celorlalţi să devină realitatea ta.” – Les Brown',
+    '“Adevărata ignoranţă nu este absenţa cunoaşterii, ci refuzul de a o dobândi.” – Karl Popper',
+    '“Trebuie să-ţi doreşti să fii puternic. Altfel, nu vei ajunge niciodată aşa.” – Friedrich Nietzsche',
+    '“Copiii găsesc totul în nimic, oamenii găsesc nimic în tot.” – Giacomo Leopardi',
+    '“Secretul geniului este transferul spiritului copilăriei în maturitate.” – Thomas Henry Huxley',
+    '“Atâta timp cât nu-ţi pasă unde eşti, nu te poţi rătăci.” - Legile lui Murphy',
+    '“Convingerea este de multe ori mai eficientă decât forţa.” – Esop',
+    '“În viață nu primești niciodată ceea ce meriți, ci ceea ce negociezi.” – Thomas Edison',
+    '“Mulţi dintre cei care eşuează o fac pentru că nu şi-au dat seama cât de aproape au fost de succes înainte de a renunţa.” – Thomas Edison',
+    '“Să pari slab atunci când eşti puternic şi puternic atunci când eşti slab.” – Sun Tzu',
+    '“În momentul în care te mulţumeşti cu mai puţin decât meriţi, vei primi chiar mai puţin decât atât.” – Maureen Dowd'
+];
+
+var text = quotes[Math.floor(Math.random() * quotes.length)];
+
+document.getElementById("quote").innerHTML = text;
